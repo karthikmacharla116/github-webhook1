98cc119e36b43a65bf7a513f1e74195a52b5b7ab
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-09-04 09:23:00 +0530

    Update GitHubSample.java

--- GitHubSample.java
+++ GitHubSample.java
@@ -1,5 +1,5 @@
 public class GitHubSample {
   public static void main(String[] args) {
-    System.out.println("Jenkins triggered - ");
+    System.out.println("Jenkins triggered - webhook1");
   }
 }
